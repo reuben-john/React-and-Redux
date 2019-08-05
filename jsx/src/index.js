7d30bf5dd8e3b1aa09031198025115cec6b34b54
Reuben John <rjkoppy@gmail.com>
2019-08-05 09:17:33 -0500

    Set up linting and formating

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,11 +1,19 @@
 // Import the React and ReactDOM libraries
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Create a react component
-const App = function() {
-  return <div> Hi there!</div>;
+const App = function app() {
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'red', color: 'white' }}>Submit</button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
