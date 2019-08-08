6983c69ab7c912e209a0cb847b3cbaef68c5a858
Reuben John <rjkoppy@gmail.com>
2019-08-08 15:38:38 -0500

    Make cascading gallery view

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setspans);
+  }
+
+  setspans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10) + 1;
+
+    this.setState({ spans });
+  };
+
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
-        <img src={urls.regular} alt={description} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
