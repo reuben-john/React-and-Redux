068940499ed5e76766abc7917bf4abd4f0b6c78f
Reuben John <rjkoppy@gmail.com>
2019-08-08 06:02:28 -0500

    Add search bar

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import SearchBar from './SearchBar';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default App;
