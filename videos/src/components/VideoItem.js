578c531db96a0de675fcfb88a4163111a4e6a163
Reuben John <rjkoppy@gmail.com>
2019-08-09 16:00:40 -0500

    Add styling to video list

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,10 +1,18 @@
+import './VideoItem.css';
 import React from 'react';
 
 const VideoItem = ({ video }) => {
   return (
-    <div>
-      <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.description} />
-      {video.snippet.title}
+    <div className="video-item item">
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.description}
+      />
+      <div className="content">
+        {video.snippet.title}
+        <div className="header"></div>
+      </div>
     </div>
   );
 };
