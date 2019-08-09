3b6f33b0afef3e41bd1e9ca17514719bc2519450
Reuben John <rjkoppy@gmail.com>
2019-08-09 15:02:06 -0500

    Set up youtube api search

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,1 +1,8 @@
-const API_KEY = process.env.REACT_APPS_YOUTUBE_DATA;
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_YOUTUBE_DATA;
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: { part: 'snippet', maxResults: 5, key: API_KEY },
+});
