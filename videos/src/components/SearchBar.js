5c9382a7f3849b6335e64899005753dffe532ccf
Reuben John <rjkoppy@gmail.com>
2019-08-09 13:49:41 -0500

    Add basic search functionality

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    // TODO: Make sure we call callback from parent component
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" name="video-search" id="video-search" />
+            <input type="text" value={this.state.term} onChange={this.onInputChange} />
           </div>
         </form>
       </div>
