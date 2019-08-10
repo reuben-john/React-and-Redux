c2c3b32bd1c9f9f21fa0d9222029033f3e6d92e1
Reuben John <rjkoppy@gmail.com>
2019-08-10 06:03:56 -0500

    Make video items clickable

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,7 +4,7 @@ import VideoList from './VideoList';
 import youtube from '../api/youtube';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
     const response = await youtube.get('/search', { params: { q: term } });
@@ -12,11 +12,15 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />I have {this.state.videos.length} videos
-        <VideoList videos={this.state.videos} />
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
       </div>
     );
   }
