495003ee949f5373fca6d5d04fed067102550c2e
Reuben John <rjkoppy@gmail.com>
2019-08-11 01:21:29 -0500

    Display details of selected song

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = () => {
-  return <div>Song Detail</div>;
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Please select a song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title}
+        <br />
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
-  console.log(state.selectedSong);
   return { song: state.selectedSong };
 };
 
