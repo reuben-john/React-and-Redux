4b8889eb38f0f33822107eefa371ba59501e24c2
Reuben John <rjkoppy@gmail.com>
2019-08-13 18:44:47 -0500

    Fix action to only pull in relevant post data

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,5 +3,5 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
-  dispatch({ fetch: 'FETCH_POSTS', payload: response });
+  dispatch({ fetch: 'FETCH_POSTS', payload: response.data });
 };
