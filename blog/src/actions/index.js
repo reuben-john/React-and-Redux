20206036950962f41dbc42dd82e366b2f273256e
Reuben John <rjkoppy@gmail.com>
2019-08-12 21:36:56 -0500

    Add redux thunk middleware

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,8 +1,7 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async () => {
-  return {
-    type: 'FETCH_POSTS',
-    payload: response,
-  };
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({ fetch: 'FETCH_POSTS', payload: response });
 };
