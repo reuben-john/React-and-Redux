a3b292aee4142d3ac4e19e1afcbef242d6937b24
Reuben John <rjkoppy@gmail.com>
2019-08-16 21:02:13 -0500

    Add signed in to state

--- twitch/client/src/components/GoogleAuth.js
+++ twitch/client/src/components/GoogleAuth.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId: '968733675934-05k7stq27q1lurq1fgidkg0udjk48c1c.apps.googleusercontent.com',
-        scope: 'email',
-      });
+      window.gapi.client
+        .init({
+          clientId: '968733675934-05k7stq27q1lurq1fgidkg0udjk48c1c.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.isSignedIn.get() });
+        });
     });
   }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
