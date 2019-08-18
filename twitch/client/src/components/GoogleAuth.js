2174f8cdb659b92ec23d07395ef22725234a36e6
Reuben John <rjkoppy@gmail.com>
2019-08-18 12:29:01 -0500

    Add react-redux to google auth

--- twitch/client/src/components/GoogleAuth.js
+++ twitch/client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
   state = { isSignedIn: null };
@@ -56,4 +58,7 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default GoogleAuth;
+export default connect(
+  null,
+  { signIn, signOut }(GoogleAuth),
+);
