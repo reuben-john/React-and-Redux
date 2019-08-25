4b4fc270c3426f4e38c5a32e503e12f48fc0ea17
Reuben John <rjkoppy@gmail.com>
2019-08-25 13:23:21 -0500

    Finish error handling

--- twitch/client/src/components/streams/StreamCreate.js
+++ twitch/client/src/components/streams/StreamCreate.js
@@ -2,20 +2,30 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label }) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+  renderInput = ({ input, label, meta }) => {
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   onSubmit(formValues) {}
 
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput} label="Enter Description" />
         <button className="ui button primrary">Submit</button>
@@ -39,4 +49,5 @@ const validate = formValues => {
 
 export default reduxForm({
   form: 'streamCreate',
+  validate,
 })(StreamCreate);
